61a6f29935a5a095510a617d95afb874b1ca8ea5
Mohamed Ali Nasr <mohamedalinasr4444@gmail.com>
2024-01-12 11:40:34 +0200

    replace backend url

--- client/src/pages/Customizer.tsx
+++ client/src/pages/Customizer.tsx
@@ -53,15 +53,18 @@ const Customizer = () => {
     try {
       setGeneratingImg(true);
 
-      const response = await fetch("http://localhost:8000/api/v1/dalle", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          prompt,
-        }),
-      });
+      const response = await fetch(
+        "https://threejs-ai-project-3c5x.onrender.com/api/v1/dalle",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            prompt,
+          }),
+        }
+      );
 
       const data = await response.json();
 
